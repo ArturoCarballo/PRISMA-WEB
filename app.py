7f4a9195a126e8fc5afe6a1d7eefede59578dd5c
ArturoCarballoArias <a01662245@tec.mx>
2024-01-26 15:35:44 -0600

    Conexion Base de datos

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
+<<<<<<< Updated upstream
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
+=======
+from flask import Flask, render_template, request, send_from_directory
+from dotenv import load_dotenv
+>>>>>>> Stashed changes
 import os
+from supabase.client import create_client, Client
 
+
+# Cargar Variables de Entorno
+load_dotenv()
+
+# Inicializar Flask
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'uploads'
+
+# Obtener las credenciales de Supabase
+supabase_url = os.getenv('SUPABASE_URL')
+supabase_key = os.getenv('SUPABASE_KEY')
+
+#Validacion si no detecta los valores
+if supabase_url is None or supabase_key is None:
+    raise ValueError("Las variables de entorno SUPABASE_URL y SUPABASE_KEY deben estar definidas.")
+
+# Crear la instancia del cliente de Supabase
+supabase: Client = create_client(supabase_url, supabase_key)
+
+def get_fuentes():
+    data = supabase.table("fuentes").select("*").execute()
+
+    return data.data
 
 @app.route('/')
 def index():
-    return render_template('upload.html')
+    try:
+        fuentes_data = get_fuentes()
+    except ValueError as e:
+        return str(e), 500
 
+<<<<<<< Updated upstream
 @app.route('/upload', methods=['POST'])
 def upload_files():
     if request.method == 'POST':
@@ -26,6 +56,9 @@ def upload_files():
             return 'Please select two files before uploading.'
     return 'Failed to upload files'
 
+=======
+    return render_template('index.html', fuentes=fuentes_data)
+>>>>>>> Stashed changes
 
 if __name__ == '__main__':
     app.run(debug=True)
