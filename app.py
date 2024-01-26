4455967ad9de8488fa9d0e6dbfa2201fbbf95a37
ArturoCarballoArias <a01662245@tec.mx>
2024-01-26 15:39:58 -0600

    Conexion Base de datos

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-<<<<<<< Updated upstream
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
-
-=======
 from flask import Flask, render_template, request, send_from_directory
 from dotenv import load_dotenv
->>>>>>> Stashed changes
 import os
 from supabase.client import create_client, Client
 
@@ -39,26 +33,8 @@ def index():
     except ValueError as e:
         return str(e), 500
 
-<<<<<<< Updated upstream
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    if request.method == 'POST':
-        file1 = request.files['file1']
-        file2 = request.files['file2']
-
-        if file1.filename and file2.filename:
-            filename1 = secure_filename(file1.filename)
-            filename2 = secure_filename(file2.filename)
-            file1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename1))
-            file2.save(os.path.join(app.config['UPLOAD_FOLDER'], filename2))
-            return 'Files uploaded successfully'
-        else:
-            return 'Please select two files before uploading.'
-    return 'Failed to upload files'
-
-=======
     return render_template('index.html', fuentes=fuentes_data)
->>>>>>> Stashed changes
+
 
 if __name__ == '__main__':
     app.run(debug=True)
